6bac8f95e14fd15b6c53b3b254753c39601ceef8
kozak201201 <kozak20012001@gmail.com>
2021-12-21 14:34:38 +0200

    fix csvReader.js

--- csvReader.js
+++ csvReader.js
@@ -5,7 +5,10 @@ async function getArrFromCSV(filePath) {
     let stream = fs.createReadStream(filePath)
     let reader = readline.createInterface({ input: stream })
     let arr = []
-    reader.on("line", (row) => { arr.push(row.split(",")) })
+    reader.on("line", (row) => {
+        row = row.replaceAll('"', '');
+        arr.push(row.split(","))
+    })
     return new Promise((resolve) => {
         reader.on('close', () => {
             resolve(arr);
